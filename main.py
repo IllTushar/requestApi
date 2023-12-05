d28c2a758f2151a12e5e10f007496b9b74c38f93
IllTushar <gtushar7668@gmail.com>
2023-12-06 00:15:52 +0530

    6.12.23x4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import requests
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
 import io
 
 class UserViewerApp:
@@ -9,9 +9,9 @@ class UserViewerApp:
         self.root.title("User Viewer App")
         self.avatar_images = []  # Store PhotoImage objects here
 
-        # Create a Listbox to display user data
-        self.listbox = tk.Listbox(root, width=50)
-        self.listbox.pack(padx=10, pady=10)
+        # Create a Frame to hold user widgets
+        self.user_frame = tk.Frame(root)
+        self.user_frame.pack(padx=10, pady=10)
 
         # Add a button to fetch and display user data
         self.fetch_button = tk.Button(root, text="Fetch Users", command=self.fetch_users)
@@ -24,28 +24,29 @@ class UserViewerApp:
 
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
-            # Clear existing data in the Listbox and reset PhotoImage objects
-            self.listbox.delete(0, tk.END)
+            # Clear existing data in the user frame and reset PhotoImage objects
+            for widget in self.user_frame.winfo_children():
+                widget.destroy()
+
             self.avatar_images = []
 
-            # Parse and display user data in the Listbox
+            # Parse and display user data in the user frame
             data = response.json().get("data", [])
             for user in data:
                 user_info = f"{user['first_name']} {user['last_name']} - {user['email']}"
-                self.listbox.insert(tk.END, user_info)
 
                 # Load and display avatar
                 avatar_url = user['avatar']
                 avatar_index = self.load_avatar(avatar_url)
                 if avatar_index is not None:
-                    # Display the avatar image using a Label
-                    avatar_label = tk.Label(self.root, image=self.avatar_images[avatar_index])
-                    avatar_label.pack()
+                    # Create a custom widget for each user
+                    user_widget = UserWidget(self.user_frame, user_info, self.avatar_images[avatar_index])
+                    user_widget.pack()
 
         else:
             # Display an error message if the request was not successful
-            self.listbox.delete(0, tk.END)
-            self.listbox.insert(tk.END, f"Error: {response.status_code}")
+            error_label = tk.Label(self.user_frame, text=f"Error: {response.status_code}")
+            error_label.pack()
 
     def load_avatar(self, avatar_url):
         try:
@@ -59,6 +60,14 @@ class UserViewerApp:
             # Resize the image if needed using LANCZOS filter
             img = img.resize((50, 50), Image.LANCZOS)
 
+            # Create a circular mask
+            mask = Image.new("L", img.size, 0)
+            draw = ImageDraw.Draw(mask)
+            draw.ellipse((0, 0, img.size[0], img.size[1]), fill=255)
+
+            # Apply the circular mask to the image
+            img.putalpha(mask)
+
             # Convert to PhotoImage for Tkinter
             photo_image = ImageTk.PhotoImage(img)
 
@@ -70,6 +79,18 @@ class UserViewerApp:
         except Exception as e:
             print(f"Error loading avatar: {e}")
             return None
+
+class UserWidget(tk.Frame):
+    def __init__(self, parent, user_info, avatar_image):
+        super().__init__(parent)
+
+        # Create a Label for user information
+        info_label = tk.Label(self, text=user_info)
+        info_label.pack(side=tk.LEFT, padx=5)
+
+        # Create a Label for the circular avatar image
+        avatar_label = tk.Label(self, image=avatar_image)
+        avatar_label.pack(side=tk.LEFT, padx=5)
 
 if __name__ == "__main__":
     root = tk.Tk()
